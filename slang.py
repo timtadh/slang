d1516529aec1a9dc669503fd44db5634e88e6eef
Tim Henderson <tim.tadh@gmail.com>
2010-12-14 13:38:33 -0500

    byte code interpreter seems to work
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- slang.py
+++ slang.py
@@ -6,92 +6,192 @@
 
 import functools
 
-def operator(f):
-    setattr(f, 'operator', None)
-    return f
+LOAD = 0x0
+SAVE = 0x1
+IMM  = 0x2
+J    = 0x3
+PC   = 0x4
+ADD  = 0x5
+SUB  = 0x6
+MUL  = 0x7
+DIV  = 0x8
+EXIT = 0x9
+PRIN = 0xa
 
+A = [J, PC]
+B = [LOAD, IMM, ADD, SUB, MUL, DIV]
+C = [SAVE]
 
-@operator
-def add(a):
-    def _add(b):
-        return a+b
-    return _add
+def load(regs, stack, pc, r1, r2):
+    regs[r1] = stack[regs[r2]]
+    return pc + 1
+def save(regs, stack, pc, r1, r2):
+    print r1, r2, ':',  regs[r1], regs[r2]
+    print len(stack), len(stack) == regs[r2]
+    if len(stack) == regs[r2]:
+        stack.append(regs[r1])
+        print stack
+    elif len(stack) < regs[r2]:
+        raise Exception, "Address out of range"
+    else:
+        stack[regs[r2]] = regs[r1]
+    return pc + 1
+def imm(regs, stack, pc, reg, immediate):
+    regs[reg] = immediate
+    return pc + 1
+def j(regs, stack, pc, reg, addr):
+    return addr
+def pc(regs, stack, pc, reg, addr):
+    regs[reg] = pc + 1
+    return pc + 1
+def add(regs, stack, pc, r1, r2):
+    print '->', regs[r1], regs[r2], regs[r1] + regs[r2]
+    regs[r1] += regs[r2]
+    return pc + 1
+def sub(regs, stack, pc, r1, r2):
+    regs[r1] -= regs[r2]
+    return pc + 1
+def mul(regs, stack, pc, r1, r2):
+    regs[r1] *= regs[r2]
+    return pc + 1
+def div(regs, stack, pc, r1, r2):
+    regs[r1] /= regs[r2]
+    return pc + 1
+def prin(regs, stack, pc, r1, r2):
+    print '>>>>>', regs[r1]
+    return pc + 1
 
-@operator
-def sub(a):
-    def _sub(b):
-        return a-b
-    return _sub
+INSTS = {
+    LOAD:load, SAVE:save, IMM:imm, J:j, PC:pc, ADD:add, SUB:sub, MUL:mul,
+    DIV:div, PRIN:prin
+}
 
-@operator
-def mul(a):
-    def _mul(b):
-        return a*b
-    return _mul
+def run(program):
+    regs = [0, 0, 0, 0, 0]
+    stack = list()
+    pc = 0
+    inst = program[pc]
+    while True:
+        print 'pc =', pc
+        print 'inst =', inst
+        print 'regs =', regs
+        print 'stack =', stack
+        op = inst[0]
+        if op == EXIT: break
+        if op in A:
+            reg = inst[1]
+            addr = None
+        elif op in B:
+            reg = inst[1]
+            addr = inst[2]
+        else:
+            reg = inst[2]
+            addr = inst[1]
+        pc = INSTS[op](regs, stack, pc, reg, addr)
+        inst = program[pc]
+        print
 
-@operator
-def div(a):
-    def _div(b):
-        return a/b
-    return _div
-
-@operator
-def _str(a):
-    return str(a)
-
-@operator
-def _print(a):
-    print a,
-    return a
-
-@operator
-def _println(b):
-    print b
-    return b
-
-ops = dict((name.lstrip('_'), obj)
-        for name,obj in locals().iteritems()
-        if hasattr(obj, 'operator')
-    )
-
-def tokenize(s, ops):
-    def typify(s):
-        if   s == '(': return ('o_paren', s)
-        elif s == ')': return ('c_paren', s)
-        elif s in ops: return ('func', ops[s])
-        try: return ('int', int(s))
-        except:
-            return ('str', s)
-    return (typify(s) for s in s.split(' '))
-
-def run(tokens):
-    def inner(c, tokens):
-        objs = list()
-        while c[0] != 'c_paren':
-            if c[0] == 'o_paren': c, n = expr(c, tokens)
-            else:
-                n = c[1]
-                c = tokens.next()
-            objs.append(n)
-        assert len(objs) <= 2
-        #print objs
-        if len(objs) == 1:
-            return c, objs[0]
-        elif len(objs) == 2:
-            r = objs[0](objs[1])
-        return c, r
-    def expr(c, tokens):
-        if c[0] != 'o_paren': raise Exception, 'no open paren'
-        c, expr = inner(tokens.next(), tokens)
-        if c[0] != 'c_paren': raise Exception, 'no close paren'
-        try: c = tokens.next()
-        except StopIteration: c = None
-        return c, expr
-    c, ret = expr(tokens.next(), tokens)
-    if c is not None: raise Exception, 'Not all tokens consumed'
-    #print
-    #print
-    #print ret
 
 if __name__ == '__main__':
-    run(tokenize('( ( add ( println ( str ( ( sub 1 ) 2 ) ) ) ) ( println hello ) )', ops))
+    run([
+#init prog
+        (IMM, 4, 0),
+        (IMM, 3, 0),
+        (SAVE, 3, 4),
+        (IMM, 3, 1),
+        (SAVE, 3, 4),
+        (IMM, 3, 2),
+        (SAVE, 3, 4),
+        (IMM, 1, 3),
+# start prog
+        (IMM, 4, 0),
+        (ADD, 4, 1),
+        (IMM, 3, 29),
+        (SAVE, 4, 3, 'save arg 1'),
+        (IMM, 3, 1),
+        (ADD, 4, 3),
+        (IMM, 3, 37),
+        (SAVE, 4, 3, 'save arg 2'),
+        (ADD, 4, 4),
+
+#func add
+    #stack save
+        (IMM, 4, 0),
+        (ADD, 4, 1),
+        (IMM, 3, 2),
+        (ADD, 4, 3, 'reg[4] += 2'),
+        (SAVE, 4, 0, 'stack save: save bp'),
+        (IMM, 3, 1),
+        (ADD, 4, 3, 'reg[4] += 1'),
+        (SAVE, 4, 1, 'stack save: save fp'),
+        (ADD, 4, 3, 'reg[4] += 1'),
+        (SAVE, 4, 2, 'stack save: save ra'),
+        (ADD, 4, 3, 'reg[4] += 1'),
+        (IMM, 0, 0),
+        (ADD, 0, 1, 'mv fp to bp'),
+        (IMM, 1, 0),
+        (ADD, 1, 4, 'mv $4 to fp'),
+    # start func body
+        (IMM, 3, 0), # load args
+        (ADD, 3, 0),
+        (IMM, 4, 1),
+        (ADD, 4, 0),
+        (LOAD, 3, 3),
+        (LOAD, 4, 4),
+        (ADD, 4, 3), # do addition
+
+        (SAVE, 1, 4), # save result
+        (IMM, 3, 1),
+        (ADD, 1, 3, 'reg[1] += 1'),
+
+        (IMM, 3, 15),
+        (ADD, 4, 3),
+
+        (SAVE, 1, 4), # save result
+        (IMM, 3, 1),
+        (ADD, 1, 3, 'reg[1] += 1'),
+    # end func body
+    #stack restore
+        (IMM, 4, 0),
+        (ADD, 4, 0, 'load bp into 4'),
+        (IMM, 3, 2),
+        (ADD, 4, 3, 'reg[4] += 2'),
+        (LOAD, 0, 4, 'stack restore: bp'),
+        (IMM, 3, 2),
+        (ADD, 4, 3, 'reg[4] += 2'),
+        (LOAD, 2, 4, 'stack restore: ra'),
+        (IMM, 3, 1),
+        (SUB, 4, 3, 'reg[4] += 1'),
+        (IMM, 3, 0),
+        (ADD, 3, 1),
+        (LOAD, 1, 4, 'stack restore: fp'),
+        (IMM, 4, 2),
+        (SUB, 3, 4),
+        (LOAD, 4, 3, 'function return loaded'),
+        (SAVE, 1, 4, 'save result'), # save result
+        (IMM, 4, 1),
+        (ADD, 1, 4, 'inc fp'),
+
+        (IMM, 4, 1),
+        (ADD, 3, 4),
+        (LOAD, 4, 3, 'function return loaded'),
+        (SAVE, 1, 4, 'save result'), # save result
+        (IMM, 4, 1),
+        (ADD, 1, 4, 'inc fp'),
+#end func
+        (IMM, 3, 0),
+        (ADD, 3, 1),
+        (IMM, 4, 2),
+        (SUB, 3, 4),
+        (LOAD, 3, 3),
+        (PRIN, 3, 3),
+
+        (IMM, 3, 0),
+        (ADD, 3, 1),
+        (IMM, 4, 1),
+        (SUB, 3, 4),
+        (LOAD, 3, 3),
+        (PRIN, 3, 3),
+    #exit
+        (EXIT, 0, 0),
+    ])
