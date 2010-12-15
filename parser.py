7c6c2a889be135d9d42b00d3a56a3f2088bf6d92
Tim Henderson <tim.tadh@gmail.com>
2010-12-15 02:57:44 -0500

    simple AST constructed using a tuple representation
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- parser.py
+++ parser.py
@@ -16,7 +16,7 @@ class Parser(object):
     def __new__(cls, **kwargs):
         ## Does magic to allow PLY to do its thing.
         self = super(Parser, cls).__new__(cls, **kwargs)
-        self.names = dict()
+        self.table = dict()
         self.yacc = yacc.yacc(module=self, **kwargs)
         return self.yacc
 
@@ -25,55 +25,71 @@ class Parser(object):
 
     def p_Start(self, t):
         'Start : Block'
-        #t[0] = t[1]
+        t[0] = t[1]
 
     def p_Block1(self, t):
         'Block : Block Stmt'
+        t[0] = t[1] + [t[2]]
 
     def p_Block2(self, t):
         'Block : Stmt'
+        t[0] = [t[1]]
 
     def p_Stmt1(self, t):
         'Stmt : NAME EQUAL FUNC LPAREN Dparams RPAREN LCURLY Block Return RCURLY'
+        t[0] = ('ASSIGN', t[1], ('FUNC', [5], t[8], t[9]))
 
     def p_Stmt2(self, t):
         'Stmt : NAME EQUAL FUNC LPAREN RPAREN LCURLY Block Return RCURLY'
+        t[0] = ('ASSIGN', t[1], ('FUNC', [], t[7], t[8]))
 
     def p_Stmt3(self, t):
         'Stmt : NAME EQUAL Call'
+        t[0] = ('ASSIGN', t[1], t[3])
 
     def p_Stmt4(self, t):
         'Stmt : Call'
+        t[0] = t[1]
 
     def p_Return1(self, t):
         'Return : RETURN Params'
+        t[0] = ('RETURN', t[2])
 
     def p_Return2(self, t):
         'Return : CONTINUE Call'
+        t[0] = ('CONTINUE', t[2])
 
     def p_Call1(self, t):
         'Call : NAME LPAREN Params RPAREN'
+        t[0] = ('CALL', t[1], t[3])
 
     def p_Call2(self, t):
         'Call : NAME LPAREN RPAREN'
+        t[0] = ('CALL', t[1], [])
 
     def p_Dparams1(self, t):
         'Dparams : Dparams COMMA NAME'
+        t[0] = t[1] + [('NAME', t[3])]
 
     def p_Dparams2(self, t):
         'Dparams : NAME'
+        t[0] = [('NAME', t[1])]
 
     def p_Params1(self, t):
         'Params : Params COMMA Value'
+        t[0] = t[1] + [t[3]]
 
     def p_Params2(self, t):
         'Params : Value'
+        t[0] = [t[1]]
 
     def p_Value1(self, t):
         'Value : NAME'
+        t[0] = ('NAME', t[1])
 
     def p_Value2(self, t):
         'Value : INT'
+        t[0] = ('INT', t[1])
 
     def p_error(self, t):
         raise SyntaxError, "Syntax error at '%s', %s.%s" % (t,t.lineno,t.lexpos)
