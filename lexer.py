25f32e625aa3fdcca018c644719bd349a9146918
Tim Henderson <tim.tadh@gmail.com>
2010-12-15 02:07:11 -0500

    lexer based on ply will be replaced when I write the example lexing algorithm later
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

---
+++ lexer.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#Author: Tim Henderson
+#Email: tim.tadh@hackthology.com
+#For licensing see the LICENSE file in the top level directory.
+
+
+from ply import lex
+from ply.lex import Token
+
+
+reserved = dict(
+    (word.lower(), word) for word in (
+        'RETURN', 'CONTINUE', 'FUNC',
+    )
+)
+
+tokens = reserved.values() + [
+    'NAME', 'INT', 'COMMA', 'LPAREN', 'RPAREN', 'LCURLY', 'RCURLY', 'EQUAL',
+]
+
+# Common Regex Parts
+
+D = r'[0-9]'
+L = r'[a-zA-Z_]'
+H = r'[a-fA-F0-9]'
+E = r'[Ee][+-]?(' + D + ')+'
+
+
+## Normally PLY works at the module level. I perfer having it encapsulated as
+## a class. Thus the strange construction of this class in the new method allows
+## PLY to do its magic.
+class Lexer(object):
+
+    def __new__(cls, **kwargs):
+        self = super(Lexer, cls).__new__(cls, **kwargs)
+        self.lexer = lex.lex(object=self, **kwargs)
+        return self.lexer
+
+    tokens = tokens
+
+    t_EQUAL = r'='
+    t_COMMA = r','
+    t_LPAREN = r'\('
+    t_RPAREN = r'\)'
+    t_LCURLY = r'\{'
+    t_RCURLY = r'\}'
+
+    name = '(' + L + ')((' + L + ')|(' + D + '))*'
+    @Token(name)
+    def t_NAME(self, token):
+        if token.value in reserved: token.type = reserved[token.value]
+        else: token.type = 'NAME'
+        return token
+
+    const_hex = '-?0[xX](' + H + ')+'
+    @Token(const_hex)
+    def t_CONST_HEX(self, token):
+        token.type = 'INT'
+        token.value = int(token.value, 16)
+        return token
+
+    const_dec_oct = '-?(' + D + ')+'
+    @Token(const_dec_oct)
+    def t_CONST_DEC_OCT(self, token):
+        token.type = 'INT'
+        if (len(token.value) > 1 and token.value[0] == '0'
+            or (token.value[0] == '-' and token.value[1] == '0')):
+            token.value = int(token.value, 8)
+        else:
+            token.value = int(token.value, 10)
+        return token
+
+    @Token(r'\n+')
+    def t_newline(self, t):
+        t.lexer.lineno += t.value.count("\n")
+
+    # Ignored characters
+    t_ignore = " \t"
+
+    def t_error(self, t):
+        raise Exception, "Illegal character '%s'" % t
+        t.lexer.skip(1)
+
+if __name__ == '__main__':
+    lexer = Lexer()
+    lexer.input('''
+        f = func(a, b) {
+            c = add(a,b)
+            return c
+        }
+        r = f(2,3)
+        print(r)
+    ''')
+    print [x for x in lexer]
