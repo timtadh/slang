baf865778c274bfc400272ac2c7ac3a2ea196282
Tim Henderson <tim.tadh@gmail.com>
2010-12-26 21:07:54 -0500

    refactored the generator in a 'class'
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- generator.py
+++ generator.py
@@ -18,49 +18,74 @@ prebuilt_funcs = {
     'exit':il.Func([], [])
 }
 
-def generate(root):
-    functions = dict()
-    generate.fcount = 0
-    def Params(node, objs):
-        inn = list()
-        out = list()
+class generate(object):
+
+    def __new__(cls, root):
+        self = super(generate, cls).__new__(cls)
+        self.__init__()
+        r = self.Block(root)
+        print '---'
+        parents = list()
+        for x,y in self.functions.iteritems():
+            print x
+            print ' '*4, 'symbols'
+            parents.append(y['symbols'].parent)
+            for a,b in y['symbols'].iteritems():
+                print ' '*8, a, b
+            print ' '*4, 'code', y['code']
+        for x in parents:
+            print repr(x)
+        print '---'
+        for x,y in self.objs.iteritems():
+            print x, y
+        print '---'
+        return r
+
+    def __init__(self):
+        self.functions = dict()
+        self.fcount = 0
+        self.objs = SymbolTable(prebuilt_funcs)
+
+
+    def Block(self, node):
+        codelist = list()
         for c in node.children:
-            if c.label == 'Takes':
-                for d in c.children:
-                    name = d.children[0].label
-                    typ_ = Type(d.children[1])
-                    inn.append({'name':name, 'type':typ_})
-            elif c.label == 'Returns':
-                for d in c.children:
-                    out.append(Type(d))
-        print '----->', inn, out
-        return inn, out
-    def Type(node):
-        if node.label == 'IntType':
-            return il.Int()
-        elif node.label == 'FuncType':
-            inn = list()
-            out = list()
-            for c in node.children:
-                if c.label == 'Takes':
-                    for t in c.children:
-                        inn.append({'name':None, 'type':Type(t)})
-                elif c.label == 'Returns':
-                    for t in c.children:
-                        out.append(Type(t))
-            return il.Func(inn, out)
-    def Func(node, objs, inn, out):
-        for obj in inn:
-            objs[obj['name']] = obj['type']
-        #print
-        #print
-        #TODO: parameter logic
-        block = Block(node.children[-2], objs)
-        #TODO: Return logic / Continue logic
-        #print inn, out
-        #print 'Func', objs.keys()
-        return block
-    def Assign(node, objs):
+            if c.label == 'Assign':
+                code = self.Assign(c)
+            elif c.label == 'Call':
+                code = self.Call(c, list())
+            else:
+                raise Exception, 'Unexpected node %s' % (c.label)
+        #print 'Block', objs.keys()
+        return code
+
+    def Call(self, node, returns):
+        name = node.children[0].label
+        params = (
+            node.children[1].children
+            if len(node.children) == 2
+            else list()
+        )
+        assert name in self.objs
+        typ_ = self.objs[name]
+        assert len(typ_.inn) == len(params)
+        assert len(typ_.out) == len(returns)
+        code = list()
+        print 'call -->', name
+        print ' '*4, typ_.inn
+        print ' '*4, typ_.out
+        print ' '*4, typ_
+        for i, p in enumerate(params):
+            if p.label == 'NAME':
+                pname = p.children[0].label
+                ptype = self.objs[pname]
+                rtype = typ_.inn[i]['type']
+                print pname, ptype, rtype
+            #il.Inst(il.IPRM, )
+        print
+        #print 'Call', objs.keys()
+
+    def Assign(self, node):
         #print node
         #print
         if node.children[1].label == 'Call':
@@ -72,77 +97,70 @@ def generate(root):
         elif node.children[1].label == 'Func':
             func = node.children[1]
             name = node.children[0].label
-            label = 'func_%d' % generate.fcount; generate.fcount += 1
-            inn, out = Params(func, objs)
+            label = 'func_%d' % self.fcount; self.fcount += 1
+            inn, out = self.Params(func)
             print 'assign -->', name
             print ' '*4, inn
             print ' '*4, out
-            objs[name] = il.Func(inn, out, label=label)
-            print ' '*4, objs[name]
-            fobjs = objs.push()
-            functions[label] = {
-                'code' : Func(func, fobjs, inn, out),
-                'symbols' : fobjs
+            self.objs[name] = il.Func(inn, out, label=label)
+            print ' '*4, self.objs[name]
+            self.objs = self.objs.push()
+            self.functions[label] = {
+                'code' : self.Func(func, inn, out),
+                'symbols' : self.objs
             }
+            self.objs = self.objs.pop()
             code = 'Todo Func code in assign'
         else:
             raise Exception, 'Unexpected node %s' % (node.children[1].label)
         #print 'Assign', objs.keys()
         return code
-    def Call(node, objs, returns):
-        name = node.children[0].label
-        params = (
-            node.children[1].children
-            if len(node.children) == 2
-            else list()
-        )
-        assert name in objs
-        typ_ = objs[name]
-        assert len(typ_.inn) == len(params)
-        assert len(typ_.out) == len(returns)
-        code = list()
-        print 'call -->', name
-        print ' '*4, typ_.inn
-        print ' '*4, typ_.out
-        print ' '*4, typ_
-        for i, p in enumerate(params):
-            if p.label == 'NAME':
-                pname = p.children[0].label
-                ptype = objs[pname]
-                rtype = typ_.inn[i]['type']
-                print pname, ptype, rtype
-            #il.Inst(il.IPRM, )
-        print
-        #print 'Call', objs.keys()
-    def Block(node, objs):
-        codelist = list()
+
+    def Func(self, node, inn, out):
+        for obj in inn:
+            self.objs[obj['name']] = obj['type']
+        #print
+        #print
+        #TODO: parameter logic
+        block = self.Block(node.children[-2])
+        #TODO: Return logic / Continue logic
+        #print inn, out
+        #print 'Func', objs.keys()
+        return block
+
+    def Type(self, node):
+        if node.label == 'IntType':
+            return il.Int()
+        elif node.label == 'FuncType':
+            inn = list()
+            out = list()
+            for c in node.children:
+                if c.label == 'Takes':
+                    for t in c.children:
+                        inn.append({'name':None, 'type':self.Type(t)})
+                elif c.label == 'Returns':
+                    for t in c.children:
+                        out.append(self.Type(t))
+            return il.Func(inn, out)
+
+    def Params(self, node):
+        inn = list()
+        out = list()
         for c in node.children:
-            if c.label == 'Assign':
-                code = Assign(c, objs)
-            elif c.label == 'Call':
-                code = Call(c, objs, list())
-            else:
-                raise Exception, 'Unexpected node %s' % (c.label)
-        #print 'Block', objs.keys()
-        return code
-    objs = SymbolTable(prebuilt_funcs)
-    r = Block(root, objs)
-    print '---'
-    parents = list()
-    for x,y in functions.iteritems():
-        print x
-        print ' '*4, 'symbols'
-        parents.append(y['symbols'].parent)
-        for a,b in y['symbols'].iteritems():
-            print ' '*8, a, b
-        print ' '*4, 'code', y['code']
-    for x in parents:
-        print repr(x)
-    print '---'
-    for x,y in objs.iteritems():
-        print x, y
-    print '---'
-    return r
+            if c.label == 'Takes':
+                for d in c.children:
+                    inn.append({
+                        'name': d.children[0].label,
+                        'type': self.Type(d.children[1])
+                    })
+            elif c.label == 'Returns':
+                for d in c.children:
+                    out.append(self.Type(d))
+        print '----->', inn, out
+        return inn, out
+
+    # ------------------------------------------------------------------------ #
+
 
 if __name__ == '__main__':
 
