b229656d19f49a659f7fcac7d78f95c1b48c3938
Tim Henderson <tim.tadh@gmail.com>
2011-07-18 09:51:06 -0400

    stubbed out files for control flow analysis
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

---
+++ cf_analyzer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#Author: Tim Henderson
+#Email: tim.tadh@hackthology.com
+#For licensing see the LICENSE file in the top level directory.
+
+
+class analyze(object):
+
+    def __new__(cls):
+        self = super(generate, cls).__new__(cls)
+        self.__init__()
+
+        return None
+
+    def __init__(self):
+        pass
+
+
---
+++ t_cf_analyzer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#Author: Tim Henderson
+#Email: tim.tadh@hackthology.com
+#For licensing see the LICENSE file in the top level directory.
+
