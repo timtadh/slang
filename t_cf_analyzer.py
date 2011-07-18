1f93adc10b18171734099717e4fd76f3784035fe
Tim Henderson <tim.tadh@gmail.com>
2011-07-18 14:49:40 -0400

    more testing of acyclic chain detection
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- t_cf_analyzer.py
+++ t_cf_analyzer.py
@@ -20,12 +20,23 @@ def analyze(s):
 def mock():
     return cf.analyze.__mock__()
 
-def chain():
-    b1 = il.Block('b1')
-    b2 = il.Block('b2')
-    b3 = il.Block('b3')
-    b4 = il.Block('b4')
-    b5 = il.Block('b5')
+def I():
+    def gen():
+        x = 0
+        while True:
+            x += 1
+            yield x
+    g = gen()
+    def i():
+        return g.next()
+    return i
+
+def chain(i):
+    b1 = il.Block('b%i' % i())
+    b2 = il.Block('b%i' % i())
+    b3 = il.Block('b%i' % i())
+    b4 = il.Block('b%i' % i())
+    b5 = il.Block('b%i' % i())
 
     b1.next += [ b2 ]
     b2.next += [ b3 ]
@@ -39,16 +50,32 @@ def chain():
 
     return [b1, b2, b3, b4, b5], b3, 4, 2
 
-def if_then():
-    b1 = il.Block('b1')
-    b2 = il.Block('b2')
-    b3 = il.Block('b3')
+def if_then(i):
+    b1 = il.Block('b%i' % i())
+    b2 = il.Block('b%i' % i())
+    b3 = il.Block('b%i' % i())
 
     b1.next += [ b2, b3 ]
     b2.next += [ b3 ]
 
     b2.prev += [ b1 ]
     b3.prev += [ b2, b3 ]
+
+    return [b1, b2, b3], b1, 2, 0
+
+def if_then_else(i):
+    b1 = il.Block('b%i' % i())
+    b2 = il.Block('b%i' % i())
+    b3 = il.Block('b%i' % i())
+    b4 = il.Block('b%i' % i())
+
+    b1.next += [ b2, b3 ]
+    b2.next += [ b4 ]
+    b3.next += [ b4 ]
+
+    b2.prev += [ b1 ]
+    b3.prev += [ b1 ]
+    b4.prev += [ b2, b3 ]
 
     return [b1, b2, b3], b1, 2, 0
 
@@ -66,24 +93,46 @@ def join(*grps):
 
     for f in grps[1:]:
         w,x,y,z = f
+        e = blks[-1]
+        s = w[0]
+        e.next += [ s ]
+        s.prev += [ e ]
         blks += w
         postmax += (y+1)
 
     return blks, cblk, postmax, postctr
 
 def t_acyclic_chain():
-    blks, cblk, postmax, postctr = chain()
+    i = I()
+    blks, cblk, postmax, postctr = chain(i)
     rtype, nset = mock().acyclic(blks, cblk)
     assert rtype == cfs.CHAIN
     assert nset == set(blks)
 
-
-def t_acyclic_chain_ifthen():
-    _chain = chain()
-    blks, cblk, postmax, postctr = join(_chain, if_then())
+def t_acyclic_chain():
+    i = I()
+    blks, cblk, postmax, postctr = join(chain(i), chain(i))
     rtype, nset = mock().acyclic(blks, cblk)
     assert rtype == cfs.CHAIN
-    assert nset == set(_chain[0])
+    assert nset == set(blks)
+
+def t_acyclic_chain_ifthen():
+    i = I()
+    _chain = chain(i)
+    _if_then = if_then(i)
+    blks, cblk, postmax, postctr = join(_chain, _if_then)
+    rtype, nset = mock().acyclic(blks, cblk)
+    assert rtype == cfs.CHAIN
+    assert nset == set(_chain[0] + [ _if_then[0][0] ])
+
+def t_acyclic_chain_ifthenelse():
+    i = I()
+    _chain = chain(i)
+    _if_then_else = if_then_else(i)
+    blks, cblk, postmax, postctr = join(_chain, _if_then_else)
+    rtype, nset = mock().acyclic(blks, cblk)
+    assert rtype == cfs.CHAIN
+    assert nset == set(_chain[0] + [ _if_then_else[0][0] ])
 
 def t_expr_const():
     raise nose.SkipTest
