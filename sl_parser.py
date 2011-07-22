f724411f2ba81cbece679aff257984f9868c0d92
Tim Henderson <tim.tadh@gmail.com>
2011-07-22 13:47:34 -0400

    generated example AST
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- sl_parser.py
+++ sl_parser.py
@@ -262,9 +262,24 @@ class Parser(object):
 
 if __name__ == '__main__':
     print Parser().parse('''
-        if (1 > 2) {
-            print 1
-        } else {
-            print 2
+        f = func(x) {
+            if (x > 0) {
+                if (x/2 + x/2 == x) { // then it is even
+                    c = f(x+1)
+                } else {
+                    c = f(x-3)
+                }
+            } else {
+                if (x < 5) {
+                    if (x != 0) {
+                        x = x - 1
+                    }
+                    c = x
+                } else {
+                    c = 10
+                }
+            }
+            return c
         }
+        print f(10)
     ''', lexer=Lexer()).dotty()
