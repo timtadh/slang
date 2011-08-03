170aed640fa54bd1a13a757b787f11371f0ead27
Tim Henderson <tim.tadh@gmail.com>
2011-08-03 11:46:02 -0400

    added if-then test for dataflow
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- df/t_df.py
+++ df/t_df.py
@@ -169,4 +169,43 @@ def t_example_ifthenelse_engine():
     assert b5_inn == set([('b2', 3), ('b2', 1), ('b2', 2), ('b2', 0)])
     assert b5_out == set([('b2', 3), ('b5', 0), ('b2', 2), ('b2', 0)])
 
-    #assert False
+
+def t_example_ifthen_engine():
+
+    blocks, functions = cf_analyze('''
+        f = func(x) {
+            c = 3
+            if (x > 0) {
+                c = f(x-1)
+            }
+            return c
+        }
+        print f(10)
+        ''')
+
+    df.forward(example.ReachingDefintions, functions)
+
+    name = example.ReachingDefintions.name
+
+    b1_inn = functions['main'].df[name].inn['b1']
+    b1_out = functions['main'].df[name].out['b1']
+    b2_inn = functions['f2'].df[name].inn['b2']
+    b2_out = functions['f2'].df[name].out['b2']
+    b3_inn = functions['f2'].df[name].inn['b3']
+    b3_out = functions['f2'].df[name].out['b3']
+    b4_inn = functions['f2'].df[name].inn['b4']
+    b4_out = functions['f2'].df[name].out['b4']
+
+    assert b1_inn == set()
+    assert b1_out == set([('b1', 0), ('b1', 3)])
+
+
+    assert b2_inn == set()
+    assert b2_out == set([('b2', 3), ('b2', 1), ('b2', 2), ('b2', 0)])
+
+    assert b3_inn == set([('b2', 3), ('b2', 1), ('b2', 2), ('b2', 0)])
+    assert b3_out == set([('b2', 3), ('b3', 4), ('b2', 2), ('b3', 0), ('b3', 1), ('b2', 0)])
+
+    assert b4_inn == set([('b2', 3), ('b3', 4), ('b2', 2), ('b3', 0), ('b3', 1), ('b2', 1), ('b2', 0)])
+    assert b4_out == set([('b2', 3), ('b3', 4), ('b2', 2), ('b2', 1), ('b3', 0), ('b3', 1), ('b2', 0)])
+
