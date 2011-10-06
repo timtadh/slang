8513405f8d185ba07e8070e515d171a086cf339f
Tim Henderson <tim.tadh@gmail.com>
2011-10-06 19:46:12 -0400

    started structuring x86 instructions
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- x86/x86.py
+++ x86/x86.py
@@ -6,6 +6,82 @@
 
 import sys
 
+## Supported x86 Operators
+opsr = (
+    'movl', 'popl', 'pushl', 'leal',
+    'addl', 'decl', 'incl',
+    'subl', 'negl',
+    'divl', 'idivl', 'mull', 'imull',
+    'cmpl',
+    'andl', 'notl', 'orl', 'xorl',
+    'sall', 'sarl', 'shll', 'shrl',
+    'int', 'call', 'ret', 'jmp',
+    'je', 'jne', 'jl', 'jnl', 'jnle', 'jle',
+        'jg', 'jng', 'jge', 'jnge',
+
+    'push', 'leave', 'call',
+    'nop',
+)
+
+## Supported x86 Registers
+regsr = (
+    'eax', 'ebx', 'ecx', 'edx', 'edi', 'esi',
+    'ebp', 'esp', 'eip', 'eflags',
+    'rax', 'rbx', 'rcx', 'rdx', 'rsi', 'rdi', 'r8',
+)
+
+_ops = dict(("_"+op, __inst(op)) for i, op in enumerate(oprs))
+regs = dict((reg, i) for i, reg in enumerate(regsr))
+sys.modules[__name__].__dict__.update(ops)
+sys.modules[__name__].__dict__.update(regs)
+
+class Inst(object):
+
+    INDENT = 4
+
+    def __init__(op, x=None, y=None):
+        '''Create an instruction argument representing the x86 instruction
+        @param op : The integer (defined in "oprs" and "_ops")
+        @param x  : The first arg. Can be anything that has a proper __str__
+                    and represents the argument
+        @param y  : The second arg.
+
+        op is required. If y exists then so must x.
+        ie. (op is not None) and ((y is not None) or (x is None))
+        '''
+
+        # assert  y is None --> x is None        logical implication
+        # all logical implications can be rewritten
+        #      X --> Y
+        #      ~X or Y
+        assert (op is not None) and ((y is not None) or (x is None))
+        self.op = op
+        self.x  = x
+        self.y  = y
+
+    def __str__(self):
+        ## We want to pretty print these instructions such that the size of
+        ## each field is fixed. So it can't just be ', '.join([op, x, y])
+
+        ## Static Info
+        indt = ' '*self.INDENT
+        op   = opsr[self.op]
+        args = [arg for arg in [self.x, self.y] if arg]
+
+        ## We only want to append ',' if they are in the middle
+        def string(s, i):
+            if i == len(args) - 1: return str(s)
+            else: return str(s) + ','
+
+        ## Create the format string, with a insert location for each argument.
+        line = (indt + '%-6s ' % op + ' '.join('%-10s' for arg in args))
+
+        ## Format the string using a special toString for each arg which
+        ## conditionally inserts the ','.
+        line = line % tuple(string(arg, i) for i, arg in enumerate(args))
+
+        return '%(indt)s%(op)s '
+
 def inst(indent, op, x=None, y=None):
     args = [arg for arg in [x, y] if arg]
     def string(s, i):
@@ -44,31 +120,4 @@ def static(lbl, base='', index='', mul=None):
 
     return '%s(%s, %s, %i)' % (lbl, base, index, mul)
 
-
-ops = dict((op, __inst(op)) for op in (
-    'movl', 'popl', 'pushl', 'leal',
-    'addl', 'decl', 'incl',
-    'subl', 'negl',
-    'divl', 'idivl', 'mull', 'imull',
-    'cmpl',
-    'andl', 'notl', 'orl', 'xorl',
-    'sall', 'sarl', 'shll', 'shrl',
-    'int', 'call', 'ret', 'jmp',
-    'je', 'jne', 'jl', 'jnl', 'jnle', 'jle',
-        'jg', 'jng', 'jge', 'jnge',
-
-    'push', 'leave', 'call',
-    'nop',
-
-
-))
-sys.modules[__name__].__dict__.update(ops)
-
-
-sys.modules[__name__].__dict__.update(dict((reg, '%'+reg) for reg in (
-    'eax', 'ebx', 'ecx', 'edx', 'edi', 'esi',
-    'ebp', 'esp', 'eip', 'eflags',
-
-    'rax', 'rbx', 'rcx', 'rdx', 'rsi', 'rdi', 'r8',
-)))
 
